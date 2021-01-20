253a8cd46c7eddcb0eb87d5ddcc89d424165e185
Daniel Imfeld <daniel@imfeld.dev>
2021-01-20 09:48:11 +0000

    Tweak install script a bit more for multiplatform

--- install.sh
+++ install.sh
@@ -10,17 +10,21 @@ if [[ ! -d ~/.tmux/plugins/tpm ]]; then
   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
 
-if [[ `which apt` ]]; then
-  sudo apt install fzf
-elif [[ `which brew` ]]; then
-  brew install fzf
-fi
-
 if [[ `which cargo` ]]; then
+  # Prefer cargo for rust utils, if it's installed
   cargo install bat bottom cargo-update exa fd-find git-delta ripgrep sd starship
+
+  if [[ `which apt` ]]; then
+    sudo apt install fzf stow
+  elif [[ `which brew` ]]; then
+    brew install fzf stow
+  fi
 elif [[ `which brew ` ]]; then
-  brew install bat bottom exa fd git-delta ripgrep sd starship
+  brew tap clementtsang/bottom
+  brew install bat bottom exa fd git-delta ripgrep sd starship fzf stow
 elif [[ `which apt ` ]]; then
-  sudo apt install bat bottom exa fd git-delta ripgrep sd starship
+  echo 'Installing starship'
+  curl -fsSL https://starship.rs/install.sh | bash
+  sudo apt install bat exa fd-find git-delta ripgrep sd fzf stow
 fi
 
