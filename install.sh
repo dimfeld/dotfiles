db694edb2a848b51abab667b71bd5a80f419bc28
Daniel Imfeld <daniel@imfeld.dev>
2021-01-20 07:15:21 +0000

    Additional vinaries and things to run

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 set -x
 
+echo "Installing zplug"
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+
+echo "Installing tmux plugin manager"
 mkdir -p ~/.tmux/plugins
 if [[ ! -d ~/.tmux/plugins/tpm ]]; then
   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
 
+sudo apt install fzf
 cargo install bat bottom cargo-update exa fd-find git-delta ripgrep sd starship
 
