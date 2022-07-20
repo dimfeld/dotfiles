d3f5bdd99a10afc3d014e1cadd35f5eb1675f6e6
Daniel Imfeld <daniel@imfeld.dev>
2022-07-19 21:12:42 -1000

    only run Prettier when a config file exists

--- nvim/.config/nvim/lua/config/formatters.lua
+++ nvim/.config/nvim/lua/config/formatters.lua
@@ -1,5 +1,38 @@
+local path = require('plenary.path');
 local util = require('formatter.util');
-local prettierd = require('formatter.defaults.prettierd');
+
+vim.env.PRETTIERD_LOCAL_PRETTIER_ONLY = 'true'
+
+local prettier_config_files = {
+  '.prettierrc',
+  '.prettierrc.js',
+  '.prettierrc.json',
+  'prettier.config.js',
+}
+
+local prettierd = function()
+  local current_path = util.get_current_buffer_file_path()
+  local current_dir = vim.fs.dirname(current_path)
+  local found_config = vim.fs.find(prettier_config_files, {
+    path = current_dir,
+    upward = true,
+    type = 'file',
+    limit = 1,
+  })
+
+  -- print(vim.inspect(found_config))
+
+  local next = next
+  if next(found_config) == nil then
+    return nil
+  end
+
+  return {
+    exe = "prettierd",
+    args = { util.escape_path(current_path) },
+    stdin = true,
+  }
+end
 
 require('formatter').setup({
   filetype = {
